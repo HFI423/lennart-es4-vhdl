56bac2be72ae7148bba01cdf18b77c0e95a2b745
Lennart Heinrich <lennart.heinrich@edu.fhdw.de>
2026-04-09 20:29:56 +0200

    add run nolib

---
+++ run-nolib.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+if [ "$#" -eq 0 ]; then
+    echo "Usage: ./run-nolib.sh ENTITY [STOP_TIME, default 1000ns]"
+    echo "Example: ./run-nolib.sh counter_tb"
+    exit 1
+fi
+
+mkdir -p work
+cd work
+
+echo "Import src and test..."
+ghdl -i --std=08 $(find ../src -name "*.vhd") $(find ../test -name "*.vhd")
+
+echo "Make $1..."
+ghdl -m --std=08 --workdir=. $1
+
+echo "Run $1..."
+ghdl -r --std=08 $1 --stop-time=${2:-1000ns} --wave=$1.ghw --assert-level=warning
+
+echo "Wave $1..."
+gtkwave $1.ghw --rcvar 'do_initial_zoom_fit yes'
