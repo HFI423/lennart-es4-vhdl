623acaa9bbcbefbd8498a7906de4310614a96878
Lennart Heinrich <lennart.heinrich@edu.fhdw.de>
2026-04-29 11:52:58 +0200

    fix counter tb

--- test/counter_tb.vhd
+++ test/counter_tb.vhd
@@ -1,76 +1,88 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
-USE ieee.numeric_std.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
  
-ENTITY counter_tb IS
-END counter_tb;
+entity counter_tb is
+end counter_tb;
  
-ARCHITECTURE behavior OF counter_tb IS 
-    
-   --Inputs
+architecture rtl of counter_tb is 
+ 
+   -- Constants
+   constant clk_period : time := 5 ns;
+
+   -- Inputs
    signal clk : std_logic := '0';
    signal rst : std_logic := '0';
    signal en : std_logic := '0';
    signal up : std_logic := '0';
 
- 	--Outputs
+ 	-- Outputs
    signal count : std_logic_vector(7 downto 0);
 
-   -- Clock period definitions
-   constant clk_period : time := 10 ns;
- 
-BEGIN
- 
-	-- Instantiate the Unit Under Test (UUT)
-   uut: entity work.counter
+begin
+
+   counter: entity work.counter
+      generic map (
+         size => count'length 
+      )
 		port map (
-          clk => clk,
-          rst => rst,
-          en => en,
-          up => up,
-          count => count
+         clk => clk,
+         rst => rst,
+         en => en,
+         up => up,
+         count => count
         );
 
-   -- Clock process definitions
-   clk_process :process
+   clk_process: process
    begin
-		clk <= '0';
-		wait for clk_period/2;
-		clk <= '1';
-		wait for clk_period/2;
+      wait for clk_period/2;
+      clk <= not clk;
    end process;
  
+   process
+   begin
 
-   -- Stimulus process
-   stim_proc: process
-   begin		
 		rst <= '1';
 		en <= '0';
 		up <= '1';
       wait for clk_period;
+      assert count = "00000000";
 		
 		rst <= '0';
 		wait for clk_period*5;
+      assert count = "00000000";
 		
 		en <= '1';
 		wait for clk_period;
+      assert count = "00000001";
 		
 		en <= '0';
 		wait for clk_period;
+      assert count = "00000001";
 		
 		en <= '1';
 		wait for clk_period*20;
+      assert count = "00010101";
 		
 		en <= '0';
-		wait for clk_period*5;
+		wait for clk_period*5; 
+      assert count = "00010101";
 		
 		rst <= '1';
 		wait for clk_period;
+      assert count = "00000000";
 		
 		rst <= '0';
 		wait for clk_period*10;
+      assert count = "00000000";
 		
 		en <= '1';
+      wait until count = "11111111";
+      wait until clk = '0';
+      wait for clk_period;
+      assert count = "00000000";
+
+      report "Done";
 
       wait;
    end process;
